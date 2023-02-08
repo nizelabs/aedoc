f91343b265a06e7c868e4c1e4c94ea2e6471b227
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2023-02-08 21:59:25 +0700

    workflows: add deno script to dispatch apy sync workflow

---
+++ scripts/actions/api-sync.ts
@@ -0,0 +1,2 @@
+// loop over changed files to reflect the changes in this repo
+// loop over deleted files to reflect the deletion in this repo
---
+++ scripts/actions/dispatch-api-sync-workflow.ts
@@ -0,0 +1,36 @@
+import invariant from "npm:tiny-invariant@^1.3.1";
+import { parse } from "std/flags/mod.ts";
+
+const { changed, deleted, branch, pat } = parse(Deno.args, {
+	default: {
+		branch: "main",
+	},
+	stopEarly: true,
+	string: ["changed", "deleted", "branch", "pat"],
+});
+
+invariant(changed, '"changed" argument is required');
+invariant(deleted, '"deleted" argument is required');
+invariant(pat, '"pat" argument is required');
+
+// send workflow dispatch request via github api
+const response = await fetch(
+	`https://api.github.com/repos/nizelabs/aedoc/actions/workflows/api-sync`,
+	{
+		body: JSON.stringify({
+			ref: branch,
+			inputs: {
+				changed: JSON.parse(changed),
+				deleted: JSON.parse(deleted),
+			},
+		}),
+		headers: {
+			accept: "application/vnd.github+json",
+			authorization: `Bearer ${pat}`,
+			"x-github-api-version": "2022-11-28",
+		},
+		method: "POST",
+	}
+);
+
+if (response.status >= 400) throw new Error(await response.json());
