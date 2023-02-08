8104485f592d7b7138681dc407308bb2a308fcd9
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2023-02-08 22:57:27 +0700

    fix(actions): include action file extension in the url

--- scripts/actions/dispatch-api-sync-workflow.ts
+++ scripts/actions/dispatch-api-sync-workflow.ts
@@ -15,7 +15,7 @@ invariant(pat, '"pat" argument is required');
 
 // send workflow dispatch request via github api
 const response = await fetch(
-	`https://api.github.com/repos/nizelabs/aedoc/actions/workflows/api-sync/dispatches`,
+	`https://api.github.com/repos/nizelabs/aedoc/actions/workflows/api-sync.yml/dispatches`,
 	{
 		body: JSON.stringify({
 			ref: branch,
